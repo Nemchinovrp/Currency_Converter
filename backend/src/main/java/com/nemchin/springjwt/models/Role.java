0101e67a4b18f277a1d5a1724014dfdfcd7f2ad3
nemchinovrp <nemchinovrp@yandex.ru>
2020-12-04 13:57:05 +0800

    init commit

---
+++ backend/src/main/java/com/nemchin/springjwt/models/Role.java
@@ -0,0 +1,25 @@
+package com.nemchin.springjwt.models;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "roles")
+public class Role {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Enumerated(EnumType.STRING)
+	@Column(length = 20)
+	private ERole name;
+
+
+	public Role(ERole name) {
+		this.name = name;
+	}
+}
